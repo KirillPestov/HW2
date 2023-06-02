dfef8333811c64510093996709e050ceb1cf4cab
Kirill Pestov <Pestov196@yandex.ru>
2023-06-02 22:36:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bonus miles =");
+
+        int amount = 13676;
+        int bonus = 20;
+        int bonusMiles = amount / bonus;
+
+        System.out.println(bonusMiles);
+    }
+}
